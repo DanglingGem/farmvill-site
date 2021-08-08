5dcde7389103c67b2955e9053c22bc81825e9783
DanglingGem <81005874+DanglingGem@users.noreply.github.com>
2021-08-08 09:37:30 +0100

    commit on main

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+// Hamburger menu
 const open = document.querySelector("#open-menu");
 const links = document.querySelector(".nav-list");
 
 
-
 open.addEventListener("click", function () {
    links.classList.toggle("show-menu");
+   
+});
+
+// Search Button
+$(function () {
+   'use strict';
+   $('i').click(function () {
+      $(this).next().animate({
+         width: '250px'
+      });
+   });
 });
