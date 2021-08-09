ae25791d9e72d7a3970470e38a18680891526b82
DanglingGem <81005874+DanglingGem@users.noreply.github.com>
2021-08-09 08:32:06 +0100

    commit on main

--- app.js
+++ app.js
@@ -8,12 +8,35 @@ open.addEventListener("click", function () {
    
 });
 
-// Search Button
-$(function () {
-   'use strict';
-   $('i').click(function () {
-      $(this).next().animate({
-         width: '250px'
+$(document).ready(function () {
+   // Search Button
+   $(function () {
+      'use strict';
+      $('i').click(function () {
+         $(this).next().animate({
+            width: '250px'
+         });
       });
    });
+
 });
+
+
+// Cart
+
+/*let count = 0;
+
+const counter = document.getElementById("counter");
+
+document.getElementById("add-animation").ad("click", function () {
+   const cl = counter.classList;
+   const c = "animated-counter";
+   count++;
+
+   counter.innerText = count;
+   cl.remove(c, cl.contains(c));
+   setTimeout(() =>
+   counter.classList.add(".animated-counter")
+   ,1)
+});*/
+
